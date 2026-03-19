b54401ce0c54ebb9734c17dfd91d03f3fba0b438
Olga Bahdanava <tomieForever@gmail.com>
2026-03-19 21:56:16 +0100

    fix mistakes

--- src/app/feature/userspace/components/sidebar/sidebar.component.ts
+++ src/app/feature/userspace/components/sidebar/sidebar.component.ts
@@ -5,8 +5,7 @@ import { ManufacturerNavigationComponent } from '../manufacturer-navigation/manu
   selector: 'app-sidebar',
   imports: [],
   template: `<div
-    class="w-[12
-      0px] flex flex-col justify-start align-middle"
+    class="w-[120px] flex flex-col justify-start align-middle"
   ></div>`,
   styleUrl: './sidebar.component.scss',
 })
